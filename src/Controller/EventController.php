012635577066e000f50840373dc3c6a88f9c7d64
apotier2022 <aymeric.potier44@outlook.fr>
2023-11-08 09:39:00 +0100

    creer sortie

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-use App\Repository\CityRepository;
+use App\Entity\Event;
+use App\Form\EventType;
+use App\Repository\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,11 +13,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
-    #[Route('/sortir', name: 'app_event')]
+    #[Route('/sortie', name: 'app_event')]
     public function index(): Response
     {
+
         return $this->render('event/index.html.twig', [
             'controller_name' => 'EventController',
+        ]);
+    }
+
+    #[Route('/sortie/creer', name: 'app_event_new')]
+    public function create(Request $request, EntityManagerInterface $entityManager, EventRepository $eventRepository): Response
+    {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $city = $form->getData();
+            $entityManager->persist($city);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_event');
+        }
+        return $this->render('event/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
