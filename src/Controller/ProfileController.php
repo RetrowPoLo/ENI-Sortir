0e807785907cc22f33930f9570e623db7bc19461
Elissalde <elissalde.dylan@gmail.com>
2023-11-09 14:24:03 +0100

    Affichage profil autre Utilisateur

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -13,37 +13,13 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile/{id}', name: 'app_profile')]
-    public function index(Request $request): Response
+    #[Route('/profile/{id}', name: 'app_profile', methods: ['GET', 'POST'])]
+    public function index(User $user): Response
     {
-        $user = $this->getUser();
-
-        if ($user->getId() == $request->get('id') || $this->isGranted('ROLE_ADMIN')) {
-        return $this->render('profile/profile.html.twig');
-        } else {
-            $this->addFlash('error', 'Vous ne pouvez pas afficher le profil d\'un autre utilisateur.');
-            return $this->redirectToRoute('app_home');
-        }
-
+        return $this->render('profile/profile.html.twig', [
+            'user' => $user
+        ]);
     }
-
-//    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-//    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-//    {
-//        $form = $this->createForm(UserType::class, $user);
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            $entityManager->flush();
-//
-//            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-//        }
-//
-//        return $this->render('user/edit.html.twig', [
-//            'user' => $user,
-//            'form' => $form,
-//        ]);
-//    }
 
     #[Route('/editProfile/{userid}', name: 'app_profile_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
